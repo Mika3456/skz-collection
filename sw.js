f63d3a999527efa06ec2c2ab7d588e1c05b91dfb
Mika3456 <skzpenstay@gmail.com>
2026-09-22 13:39:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const C='v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='skz-collection-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
